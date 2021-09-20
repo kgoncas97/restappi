140aaec82f22de36606bba62e7d4e951bfae71a1
kgoncas97 <69826822+kgoncas97@users.noreply.github.com>
2021-09-19 21:12:18 -0600

    Add files via upload

---
+++ escuela.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = express.Router();
+
+const mysqlConnection  = require('../database.js');
+
+// GET all Employees
+router.get('/', (req, res) => {
+  mysqlConnection.query('SELECT * FROM escuela', (err, rows, fields) => {
+    if(!err) {
+      res.json(rows);
+    } else {
+      console.log(err);
+    }
+  });  
+});
+
+// GET An Escuela
+router.get('/:id', (req, res) => {
+  const { id } = req.params; 
+  mysqlConnection.query('SELECT * FROM escuela WHERE id = ?', [id], (err, rows, fields) => {
+    if (!err) {
+      res.json(rows[0]);
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+// DELETE An escuela
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  mysqlConnection.query('DELETE FROM employee WHERE id = ?', [id], (err, rows, fields) => {
+    if(!err) {
+      res.json({status: 'Escuela Deleted'});
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+// INSERT An Escuela
+router.post('/', (req, res) => {
+  const {id, name, salary} = req.body;
+  console.log(id, name, salary);
+  const query = `
+    SET @id = ?;
+    SET @name = ?;
+    SET @salary = ?;
+    CALL escuelaAddOrEdit(@id, @name, @salary);
+  `;
+  mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
+    if(!err) {
+      res.json({status: 'Escuela Saved'});
+    } else {
+      console.log(err);
+    }
+  });
+
+});
+
+router.put('/:id', (req, res) => {
+  const { name, salary } = req.body;
+  const { id } = req.params;
+  const query = `
+    SET @id = ?;
+    SET @name = ?;
+    SET @salary = ?;
+    CALL escuelaAddOrEdit(@id, @name, @salary);
+  `;
+  mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
+    if(!err) {
+      res.json({status: 'Escuela Updated'});
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+
